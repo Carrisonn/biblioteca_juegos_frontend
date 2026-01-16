339d1b4f05142443887fd656b98903d69e4c68fc
Carrison <carlos97181034535@gmail.com>
2026-01-16 15:30:14 +0100

    refactor gameStore to unify message handling and improve API response management

--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -1,120 +1,129 @@
 import { create } from 'zustand'
+import { postConfig } from '../utils/postConfig.js'
+import { putConfig } from '../utils/putConfig.js'
 
-export const useGameStore = create(set => ({
+let messageTimeout
+
+export const useGameStore = create((set, get) => ({
   games: [],
   isLoading: false,
-  searchFormMessage: '',
-  manipulateFormMessage: '',
-  gameListMessage: '',
-  typeMessage: '',
+  message: '',
   editingGame: null,
   setEditingGame: game => set({ editingGame: game }),
 
   getGames: async () => {
+    set({ isLoading: true })
     const API_URL = `${import.meta.env.VITE_API_URL}/games`
+
     try {
-      set({ isLoading: true })
       const response = await fetch(API_URL)
       const data = await response.json()
-      if (!response.ok) return set({ games: [], gameListMessage: data.errorMessage, typeMessage: 'error' })
-      set({ games: data.games, gameListMessage: '', searchFormMessage: '', manipulateFormMessage: '' })
+      if (!response.ok) return set({ games: [], message: data.errorMessage })
+
+      set({ games: data.games, message: '' })
     } catch (error) {
-      console.log(error)
+      //console.log(error)
+      set({ message: 'Hubo un problema al obtener los juegos' })
     } finally {
       set({ isLoading: false })
+      get().clearMessageWithDelay()
     }
   },
 
   searchGame: async game => {
+    set({ isLoading: true })
     const API_URL = `${import.meta.env.VITE_API_URL}/search/${game}`
+
     try {
-      set({ isLoading: true })
       const response = await fetch(API_URL)
       const data = await response.json()
-      if (!response.ok) return set({ searchFormMessage: data.errorMessage, typeMessage: 'error' })
-      set({ games: data.games, searchFormMessage: '', manipulateFormMessage: '' })
+      if (!response.ok) return set({ message: data.errorMessage })
+
+      set({ games: data.games, message: '' })
     } catch (error) {
-      console.log(error)
+      //console.log(error)
+      set({ message: 'Hubo un problema al buscar el juego' })
     } finally {
       set({ isLoading: false })
+      get().clearMessageWithDelay()
     }
   },
 
   createGame: async (game, state) => {
-    const API_URL = `${import.meta.env.VITE_API_URL}/create`
-    try {
-      const POST_CONFIG = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ game, state })
-      }
+    set({ isLoading: true })
+    const { API_URL, POST_CONFIG } = postConfig(game, state)
 
-      set({ isLoading: true })
+    try {
       const response = await fetch(API_URL, POST_CONFIG)
       const data = await response.json()
-      if (!response.ok) return set({ manipulateFormMessage: data.errorMessage, typeMessage: 'error' })
+      if (!response.ok) return set({ message: data.errorMessage })
+
       set(({ games }) => ({
         games: [...games, data.newGame],
-        manipulateFormMessage: data.successMessage,
-        typeMessage: 'success'
+        message: data.successMessage
       }))
-      setTimeout(() => {
-        set({ manipulateFormMessage: '' })
-      }, 4000)
+
     } catch (error) {
-      console.log(error)
+      //console.log(error)
+      set({ message: 'Hubo un problema al crear el juego' })
     } finally {
       set({ isLoading: false })
+      get().clearMessageWithDelay()
     }
   },
 
   deleteGame: async id => {
+    set({ isLoading: true })
     const API_URL = `${import.meta.env.VITE_API_URL}/delete/${id}`
+
     try {
-      set({ isLoading: true })
       const response = await fetch(API_URL, { method: 'DELETE' })
       const data = await response.json()
-      if (!response.ok) return set({ gameListMessage: data.errorMessage, typeMessage: 'error' })
+      if (!response.ok) return set({ message: data.errorMessage })
+
       set(({ games }) => ({
         games: games.filter(game => game.id !== id),
-        gameListMessage: data.successMessage,
-        typeMessage: 'success'
+        message: data.successMessage
       }))
-      setTimeout(() => {
-        set({ gameListMessage: '' })
-      }, 4000)
+
     } catch (error) {
-      console.log(error)
+      //console.log(error)
+      set({ message: 'Hubo un problema al borrar el juego' })
     } finally {
       set({ isLoading: false })
+      get().clearMessageWithDelay()
     }
   },
 
   editGame: async (id, game, state) => {
-    const API_URL = `${import.meta.env.VITE_API_URL}/edit`
+    set({ isLoading: true })
+    const { API_URL, PUT_CONFIG } = putConfig(id, game, state)
+
     try {
-      const PUT_CONFIG = {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ id, game, state })
-      }
-      set({ isLoading: true })
       const response = await fetch(API_URL, PUT_CONFIG)
       const data = await response.json()
-      if (!response.ok) return set({ manipulateFormMessage: data.errorMessage, typeMessage: 'error' })
+      if (!response.ok) return set({ message: data.errorMessage })
+
       set(({ games }) => ({
         games: games.map(game => game.id === data.updatedGame.id ? data.updatedGame : game),
-        manipulateFormMessage: data.successMessage,
-        typeMessage: 'success'
+        message: data.successMessage
       }))
 
-      setTimeout(() => {
-        set({ manipulateFormMessage: '' })
-      }, 4000)
     } catch (error) {
-      console.log(error)
+      //console.log(error)
+      set({ message: 'Hubo un problema al editar el juego' })
     } finally {
       set({ isLoading: false })
+      get().clearMessageWithDelay()
     }
+  },
+
+  clearMessageWithDelay: () => {
+    if (messageTimeout) clearTimeout(messageTimeout)
+
+    messageTimeout = setTimeout(() => {
+      set({ message: '' })
+      messageTimeout = null
+    }, 1000)
   }
 }))
