5b508304cccd9f28ea304f36db13f3fdc6c0231a
Carrison <carlos97181034535@gmail.com>
2026-02-14 14:06:00 +0100

    feat: add useSearchForm hook for managing game search functionality

---
+++ src/hooks/useSearchForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { useGameAPI } from '../hooks/useGameAPI.jsx'
+import { useStore } from '../store/store.js'
+
+export function useSearchForm() {
+  const { searchGame, getGames } = useGameAPI()
+
+  const setMessage = useStore(state => state.setMessage)
+  const message = useStore(state => state.message)
+
+  const [inputValue, setInputValue] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const game = inputValue.trim()
+    if (!game) return
+
+    setMessage('')
+    searchGame(game)
+    setInputValue('')
+  }
+
+  function handleInputValue(event) {
+    setInputValue(event.target.value)
+  }
+
+  function handleResetList() {
+    getGames()
+    setInputValue('')
+  }
+
+  return {
+    message,
+    inputValue,
+    handleSubmit,
+    handleInputValue,
+    handleResetList
+  }
+}
