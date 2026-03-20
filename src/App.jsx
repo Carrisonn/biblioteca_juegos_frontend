28427e2c2704a09ac7e829fe858dad2208fef38a
Carrison <carlos97181034535@gmail.com>
2026-03-20 20:33:04 +0100

    added: floating button for the scroll navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { useScroll } from './hooks/useScroll.jsx'
 import { SearchForm } from './components/SearchForm.jsx'
 import { ManipulativeForm } from './components/ManipulativeForm.jsx'
 import { GameListing } from './components/GameListing.jsx'
+import { FloatingButton } from './components/FloatingButton.jsx'
 
 export default function App() {
+  const { showButton } = useScroll()
+
   return (
     <main>
       <h1>Mis <span className="primary-color">Juegos</span></h1>
@@ -17,6 +21,8 @@ export default function App() {
           <GameListing />
         </section>
       </div>
+
+      {showButton && <FloatingButton />}
     </main>
   )
 }
