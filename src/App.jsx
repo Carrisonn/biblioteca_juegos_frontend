9b2c08c6d75db81edddadf5372a656d3dd42e8f1
Carrison <carlos97181034535@gmail.com>
2026-04-02 18:50:41 +0200

    refactor: using route component for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { useScroll } from './hooks/useScroll.jsx'
-import { SearchForm } from './components/SearchForm.jsx'
-import { ManipulativeForm } from './components/ManipulativeForm.jsx'
-import { GameListing } from './components/GameListing.jsx'
-import { FloatingButton } from './components/FloatingButton.jsx'
+import { Route } from './components/Route.jsx'
+import { Home } from './components/Home.jsx'
+import { APIHealthCheck } from './components/APIHealthCheck.jsx'
 
 export default function App() {
-  const { showButton } = useScroll()
-
   return (
-    <main>
-      <h1>Mis <span className="primary-color">Juegos</span></h1>
-      <div className='container'>
-        <section className='wrapper'>
-          <SearchForm />
-          <hr />
-          <ManipulativeForm />
-        </section>
-
-        <section className='wrapper'>
-          <GameListing />
-        </section>
-      </div>
-
-      {showButton && <FloatingButton />}
-    </main>
+    <>
+      <Route path='/' component={Home} />
+      <Route path='/health' component={APIHealthCheck} />
+    </>
   )
 }
