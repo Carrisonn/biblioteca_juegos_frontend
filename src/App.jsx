513e37d187e5a3e3b63d2895e8bf9e1f12afd192
Carrison <carlos97181034535@gmail.com>
2026-04-05 22:30:52 +0200

    refactor: restructure App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { Route } from './components/Route.jsx'
-import { Home } from './components/Home.jsx'
-import { APIHealthCheck } from './components/APIHealthCheck.jsx'
+import { useScroll } from './hooks/useScroll.jsx'
+import { Summary } from './components/Summary.jsx'
+import { SearchForm } from './components/SearchForm.jsx'
+import { ManipulativeForm } from './components/ManipulativeForm.jsx'
+import { GameListing } from './components/GameListing.jsx'
+import { FloatingButton } from './components/FloatingButton.jsx'
 
 export default function App() {
+  const { showButton } = useScroll()
+
   return (
-    <>
-      <Route path='/' component={Home} />
-      <Route path='/health' component={APIHealthCheck} />
-    </>
+    <main>
+      <h1>Mis <span className="primary-color">Juegos</span></h1>
+      <div className='container'>
+        <section className='wrapper'>
+          <Summary />
+          <hr />
+          <SearchForm />
+          <hr />
+          <ManipulativeForm />
+        </section>
+
+        <section className='wrapper'>
+          <GameListing />
+        </section>
+      </div>
+
+      {showButton && <FloatingButton />}
+    </main>
   )
 }
