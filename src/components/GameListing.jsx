b50f51723b0c854b72f36902cccef99ed307fe82
Carrison <carlos97181034535@gmail.com>
2026-04-05 22:28:21 +0200

    refactor: statistics moved to its own component

--- src/components/GameListing.jsx
+++ src/components/GameListing.jsx
@@ -2,11 +2,9 @@ import { useGameListing } from '../hooks/useGameListing.jsx'
 import { useStore } from '../store/store.js'
 import { GameCard } from './GameCard.jsx'
 import { Toast } from './Toast.jsx'
-import styles from './GameListing.module.css'
 
 export function GameListing() {
-  const { games, sortedGames } = useGameListing()
-  const totalGames = useStore(state => state.totalGames)
+  const { sortedGames } = useGameListing()
   const message = useStore(state => state.message)
   const isLoading = useStore(state => state.isLoading)
 
@@ -14,16 +12,13 @@ export function GameListing() {
     <>
       {message && <Toast message={message} />}
 
-      <h2>Lista de <span className="primary-color">Juegos</span></h2>
-      <p className={styles.game_stats}>Juegos Totales: {totalGames}</p>
-      <p className={styles.game_stats}>Juegos Encontrados: {games.length}</p>
-
-      <div className={styles.game_info}>
+      <h2>Lista de <span className='primary-color'>Juegos</span></h2>
+      <div className='game-info'>
         <h3>Nombre</h3>
         <h3>Terminado</h3>
       </div>
 
-      <div className={styles.game_list}>
+      <div className='column'>
         {
           isLoading
             ? <div className="loader"></div>
