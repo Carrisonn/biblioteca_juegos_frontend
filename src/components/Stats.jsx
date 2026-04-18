1b481b66bc456e122945bf74e4d18feee5ea6368
Carrison <carlos97181034535@gmail.com>
2026-04-18 17:36:22 +0200

    refactor: take the game array from the store

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,15 +1,14 @@
-import { useGameListing } from '../hooks/useGameListing.jsx'
 import { useStore } from '../store/store.js'
 import styles from './Stats.module.css'
 
 export function Stats() {
-  const { games } = useGameListing()
   const totalGames = useStore(state => state.totalGames)
+  const games = useStore(state => state.games)
 
   return (
     <>
       <p className={styles.game_stats}>Juegos Totales: {totalGames}</p>
-      <p className={styles.game_stats}>Juegos Encontrados: {games.length}</p>
+      <p className={styles.game_stats}>Juegos Encontrados: {games?.length}</p>
     </>
   )
 }
