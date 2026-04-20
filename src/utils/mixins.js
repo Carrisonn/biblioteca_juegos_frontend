05d16dfe26323dd05b11c364545e51b04a14797c
Carrison <carlos97181034535@gmail.com>
2026-04-20 13:51:59 +0200

    add: modal config

--- src/utils/mixins.js
+++ src/utils/mixins.js
@@ -6,6 +6,19 @@ export const toast = Swal.mixin({
   showConfirmButton: false,
   timer: 6000,
   padding: '0.5rem',
-  background: '#4c00ff',
+  background: '#703bf7',
   color: '#fff',
+})
+
+export const cancelActionModal = Swal.mixin({
+  text: 'Esta acción no se puede deshacer',
+  cancelButtonText: 'Cancelar',
+  icon: 'warning',
+  background: '#141414',
+  color: '#fff',
+  showConfirmButton: true,
+  showCancelButton: true,
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+  heightAuto: false
 })
