fa3bc46cecfb4d7d9386020281e6fdedc336e638
Carrison <carlos97181034535@gmail.com>
2026-04-20 13:54:14 +0200

    refactor: update handleDeleteGame to use confirmation modal before deletion

--- src/hooks/useGamecard.jsx
+++ src/hooks/useGamecard.jsx
@@ -1,6 +1,7 @@
 import { useGameAPI } from '../hooks/useGameAPI.jsx'
 import { useStore } from '../store/store.js'
 import { useScroll } from '../hooks/useScroll.jsx'
+import { cancelActionModal } from '../utils/mixins.js'
 
 export function useGameCard() {
   const { deleteGame } = useGameAPI()
@@ -14,10 +15,15 @@ export function useGameCard() {
     setEditingGame(game)
   }
 
-  const handleDeleteGame = gameId => {
-    if (!gameId) return
+  const handleDeleteGame = async ({ game, id }) => {
+    if (!game || !id) return
 
-    deleteGame(gameId)
+    const result = await cancelActionModal.fire({
+      title: `¿Estás seguro de querer eliminar ${game}?`,
+      confirmButtonText: `Si, elimina ${game}`
+    })
+
+    if (result.isConfirmed) deleteGame(id)
   }
 
   return {
